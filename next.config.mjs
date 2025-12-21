c2a02eae8f991e5ad283ead9d77a71a572d02eb2
manya Shrivastava <manyashrivastava08@gmail.com>
2025-12-21 18:25:46 +0000

    dont patch the issue fix it from root to resolve the issue of ## Error T

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  serverActions: {
-    bodySizeLimit: '50mb',
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
   },
 };
 
